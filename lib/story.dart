00d563eec9d9ea17f04c38764cd79f83695b6741
unknown <nagarajullegaddi01@gmail.com>
2020-07-05 23:18:55 +0530

    add project

---
+++ lib/story.dart
@@ -0,0 +1,9 @@
+//Defining Class with properties storyTitle,choice1,choice2
+
+class Story{
+  String storyTitle;
+  String choice1;
+  String choice2;
+  Story({ this.storyTitle, this.choice1,this.choice2});
+
+}
